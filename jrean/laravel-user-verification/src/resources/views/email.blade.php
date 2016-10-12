a9e9d1c8703a4183ed3253c07784b398547a9a29
vasa137 <vasa.nikola@gmail.com>
2016-10-12 23:27:55 +0200

    Vendor

---
+++ jrean/laravel-user-verification/src/resources/views/email.blade.php
@@ -0,0 +1,1 @@
+Click here to verify your account: <a href="{{ $link = "http://naucikako.com/email-verification/check/$user->verification_token" . '?email=' . urlencode($user->email) }}">{{ $link }}</a>
