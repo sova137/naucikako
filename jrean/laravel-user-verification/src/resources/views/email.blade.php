2505640b5f07f834d6eb0bf557e50f93552872dd
vasa137 <vasa.nikola@gmail.com>
2016-10-13 17:51:18 +0200

    navbar sredjen

--- jrean/laravel-user-verification/src/resources/views/email.blade.php
+++
@@ -1,1 +0,0 @@
-Click here to verify your account: <a href="{{ $link = "http://naucikako.com/email-verification/check/$user->verification_token" . '?email=' . urlencode($user->email) }}">{{ $link }}</a>
